bf6e706dc73b1a41d82bb245e99f55ed2981fbe5
archis1405 <archismandas1405@gmail.com>
2026-04-03 00:53:05 +0530

    Basic Setup of application

---
+++ src/main/java/com/example/finance_dashboard_backend/FinanceDashboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.finance_dashboard_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceDashboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceDashboardBackendApplication.class, args);
+	}
+
+}
